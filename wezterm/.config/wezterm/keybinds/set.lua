34df6c9c67311fe349d32390098e81edb28eb890
Miguel Panuto <miguel.panuto@live.com>
2024-10-04 23:52:14 -0300

    updates

--- wezterm/.config/wezterm/keybinds/set.lua
+++ wezterm/.config/wezterm/keybinds/set.lua
@@ -1,57 +1,62 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local act = wezterm.action
 
 local adjustment_size = 10
 
 return {
-	{
-		key = "|",
-		mods = "LEADER|SHIFT",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "-",
-		mods = "LEADER",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "h",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Left", adjustment_size }),
-	},
-	{
-		key = "j",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Down", adjustment_size }),
-	},
-	{
-		key = "k",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Up", adjustment_size }),
-	},
-	{
-		key = "l",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Right", adjustment_size }),
-	},
-	{
-		key = "H",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "L",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "K",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "J",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Down"),
-	},
+  {
+    key = '|',
+    mods = 'LEADER|SHIFT',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Left', adjustment_size },
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Down', adjustment_size },
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Up', adjustment_size },
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Right', adjustment_size },
+  },
+  {
+    key = 'H',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'L',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'K',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'J',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'm',
+    mods = 'LEADER',
+    action = act.TogglePaneZoomState,
+  },
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -9,4 +9,4 @@ source $ZSH/oh-my-zsh.sh
 eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
 
 # Created by `pipx` on 2024-09-18 16:14:06
-export PATH="$PATH:/home/miguel/.local/bin"
+export PATH="$PATH:/home/miguel/.local/bin:/home/miguel/geckodriver"
